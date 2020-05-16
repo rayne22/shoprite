5e957abd1e80bedac1911976a9f2706f7334de3a
rayne22 <changalesa8@gmail.com>
2020-05-16 15:16:20 +0200

    initial comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Observable } from "rxjs";
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  DocumentReference,
+} from "@angular/fire/firestore";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'shoprite';
+  items: Observable<any[]>;
+  constructor(firestore: AngularFirestore) {
+    this.items = firestore.collection("items").valueChanges();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { AngularFireStorageModule, BUCKET } from "@angular/fire/storage";
+import { environment } from "../environments/environment";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDm5IkoRBkSDIg4PoU7VBrGBh5gAlnxmvQ",
+  authDomain: "shoperite-d69fa.firebaseapp.com",
+  databaseURL: "https://shoperite-d69fa.firebaseio.com",
+  projectId: "shoperite-d69fa",
+  storageBucket: "shoperite-d69fa.appspot.com",
+  messagingSenderId: "509236349940",
+  appId: "1:509236349940:web:807b0a303d9367809ec7a1",
+  measurementId: "G-094QCC34Z3",
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireStorageModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
