2c8e65639ecf1d784b1d1427fa850edb68d6c554
rayne22 <changalesa8@gmail.com>
2020-05-29 08:22:20 +0200

    changes

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CategoryModel } from "src/app/home/models/categories.model";
+import { CategoriesService } from "src/app/home/services/categories.service";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"],
 })
 export class FooterComponent implements OnInit {
+  listOfData: CategoryModel[];
+  category: CategoryModel;
+  listOfDisplayData: CategoryModel[];
 
-  constructor() { }
+  constructor(private categoryService: CategoriesService) {}
 
   ngOnInit(): void {
+    this.categoryService.getCategories().subscribe((res) => {
+      this.listOfData = res;
+      this.listOfDisplayData = this.listOfData;
+      // this.totalNumber = this.listOfData.length;
+      console.log("Categories>>>>", this.listOfDisplayData);
+    });
   }
-
 }
